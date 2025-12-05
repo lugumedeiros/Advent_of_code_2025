565c1708d7a6817c1f1e6a5662be3d94d16a9b73
Luiz Gustavo de Medeiros <lugumedeiros@gmail.com>
2025-12-04 21:47:02 -0300

    refactor: Made a bit clearer

--- day_1/main.py
+++ day_1/main.py
@@ -1,28 +1,32 @@
 class Dial:
-    def __init__(self):
-        self.pos = 50
+    def __init__(self, start_pos:int=50):
+        self.pos = start_pos
         self.count = 0
 
     def rotate(self, rotation_value:int) -> None:
+        def is_remainder_greater_than_start_to_zero(start:int, distance:int, is_going_pos:bool):
+            partial_distance = distance % 100
+            if is_going_pos:
+                to_zero = 100 - start
+            else:
+                to_zero = start
+            return partial_distance >= to_zero
+
         if rotation_value == 0:
             return
 
-        is_going_right = rotation_value > 0
+        is_going_positive = rotation_value > 0
         start = self.pos
         end = (start + rotation_value) % 100
         distance = abs(rotation_value)
+        distance_rotations = distance//100
 
         if start == 0:
-            self._increase_zero_counter(distance//100)
+            self._increase_zero_counter(distance_rotations)
         else:
-            partial_distance = distance % 100
-            if is_going_right:
-                if (partial_distance >= 100 - start):
+            if is_remainder_greater_than_start_to_zero(start, distance, is_going_positive):
                     self._increase_zero_counter(1)
-            else:
-                if (partial_distance >= start):
-                    self._increase_zero_counter(1)
-            self._increase_zero_counter(distance // 100)
+            self._increase_zero_counter(distance_rotations)
         self.pos = end
 
     def _increase_zero_counter(self, rotations:int) -> None:
@@ -45,6 +49,6 @@ if __name__ == "__main__":
             dial_value = -int(line.replace('L', ''))
         
         dial.rotate(dial_value)
-        print(f"{dial_value}: start at {x} end at {dial.pos}, zero_pass:{dial.count}")
+        print(f"Rotation: {dial_value}\t- Start at {x} end at {dial.pos}, zero_count:{dial.count}")
         
 
